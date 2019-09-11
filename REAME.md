1c710c09f3156100e8eb60c438795142b25af3bf
Stuart Bourne <sbourne7510@gmail.com>
2019-09-11 13:07:09 -0700

    Initial linear classifier (perceptron)

---
+++ REAME.md
@@ -0,0 +1,8 @@
+# Overview
+This repository houses basic neural networks written in both python and C++.
+This primarily serves as a learning experience for building networks from scratch,
+with the objective of leading to building up a convolutional neural network to 
+classify digits.
+
+First, the networks are written in python due to the ability to easily plot values using
+pyplot, then they are converted to C++ to allow for optimal computation times.
---
+++ python/simple_classification.py
@@ -0,0 +1,47 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+feature_set = np.array([[0, 1, 0], [0, 0, 1], [1, 0, 0], [1, 1, 0], [1, 1, 1]])
+labels = np.array([1, 0, 0, 1, 1])
+labels = labels.reshape(5, 1)
+
+#lets generate random weights and biases for the start
+np.random.seed(42)
+weights = np.random.rand(3, 1)
+bias = np.random.rand(1)
+lr = 0.05
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+def sigmoid_der(x):
+    return sigmoid(x) * (1 - sigmoid(x))
+
+#l now lets actually train
+for epoch in range(2000):
+    inputs = feature_set
+
+    #feedforward step 1
+    XW = np.dot(feature_set, weights) + bias
+
+    #feedforward step 2
+    z = sigmoid(XW)
+
+    #backprop step 1
+    error = z - labels
+
+    print("error: ", error.sum())
+    #backprop step 2
+    dcost_dpred =error
+    dpred_dz = sigmoid_der(z)
+
+    z_delta = dcost_dpred * dpred_dz
+    inputs = feature_set.T
+    weights -= lr * np.dot(inputs, z_delta)
+
+    for num in z_delta:
+        bias -= lr * num
+
+single_point = np.array([0, 0, 0])
+result = sigmoid(np.dot(single_point, weights) + bias)
+print (result)
